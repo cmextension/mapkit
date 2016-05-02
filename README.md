0a170529be354e959487af32345676ff59c34b68
Tran Duy Hung <nguyentieuhau@gmail.com>
2016-05-02 09:16:24 +0700

    0.0.3 - Fix #4

--- README.md
+++ README.md
@@ -4,11 +4,7 @@ Mapkit is a simple extension for [Pagekit CMS](http://pagekit.com) to display Go
 
 ## Installation
 
-In your Pagekit back-end, you navigate to System -> Extensions, click Upload button and select the zip package you download from [Github's releases](https://github.com/cmextension/mapkit/releases).
-
-## Configuration
-
-To use your Google Maps API key, you navigate to System -> Extensions, click "Mapkit" and enter your Google Maps API key into "Google Maps API Key" field.
+In your Pagekit back-end, you navigate to System -> Extensions, click Upload button and select the zip package you download from this repository.
 
 ## Usage
 
--- src/Plugin/MapkitPlugin.php
+++ src/Plugin/MapkitPlugin.php
@@ -29,22 +29,28 @@ class MapkitPlugin implements EventSubscriberInterface
 				}
 
 				$map_id = 'mapkit' . $widget->id;
-				$lat = $widget->data['lat'];
-				$lng = $widget->data['lng'];
-				$width = $widget->data['width'];
-				$height = $widget->data['height'];
-				$zoom = $widget->data['zoom'];
-				$get_direction = $widget->data['get_direction'];
+
+				$lat = (isset($widget->data['lat']) && !empty($widget->data['lat'])) ? $widget->data['lat'] : 0;
+
+				$lng = (isset($widget->data['lng']) && !empty($widget->data['lng'])) ? $widget->data['lng'] : 0;
+
+				$width = (isset($widget->data['width']) && !empty($widget->data['width'])) ? $widget->data['width'] : '100%';
+
+				$height = (isset($widget->data['height']) && !empty($widget->data['height'])) ? $widget->data['height'] : '300px';
+
+				$zoom = (isset($widget->data['zoom']) && !empty($widget->data['zoom'])) ? $widget->data['zoom'] : 10;
+
+				$get_direction = (isset($widget->data['get_direction']) && !empty($widget->data['get_direction'])) ? $widget->data['get_direction'] : 0;
 			}
 			else
 			{
 				$map_id = isset($options['id']) ? $options['id'] : 'mapkit';
-				$lat = $options['lat'];
-				$lng = $options['long'];
+				$lat = isset($options['lat']) ? $options['lat'] : 0;
+				$lng = isset($options['long']) ? $options['long'] : 0;
 				$width = isset($options['width']) ? $options['width'] : '100%';
 				$height = isset($options['height']) ? $options['height'] : '300px';
-				$zoom = isset($options['zoom']) ? $options['zoom'] : '10';
-				$get_direction = isset($options['get_direction']) ? $options['get_direction'] : '0';
+				$zoom = isset($options['zoom']) ? $options['zoom'] : 10;
+				$get_direction = isset($options['get_direction']) ? $options['get_direction'] : 0;
 			}
 
 			if ((strpos($width, 'px') === false) && (strpos($width, '%') === false))
